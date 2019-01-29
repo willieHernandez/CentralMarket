54d0629fa7a505ecb23063bfb40b1a46c19ebfd6
willieHernandez <guillermo.w.hernandezjr@gmail.com>
2019-01-29 01:45:04 -0600

    Made a change to recommend a beer so that way it rendered correctly.

--- src/components/FormComponents/RecommendBeer.js
+++ src/components/FormComponents/RecommendBeer.js
@@ -20,73 +20,78 @@ class RecommendBeer extends Component{
 
         return(
             <Fragment>
-                <Header />
-                <Formik
-                    onSubmit={(values, {setSubmitting})=>{
-                        console.log("Inisde the on submit method")
-                        this.setState({
-                            formSubmitted:true
-                        })
-                    }}
+                <div className="container">
+                    <Header />
+                    <div className="body-portion">
+                        <Formik
+                            onSubmit={(values, {setSubmitting})=>{
+                                console.log("Inisde the on submit method")
+                                this.setState({
+                                    formSubmitted:true
+                                })
+                            }}
 
-                    initialValues ={{
-                        firstName:"",
-                        lastName:"",
-                        email:"",
-                        isLocalBrewery:false,
-                        beerName:"",
-                        breweryName:""
-                    }}
+                            initialValues ={{
+                                firstName:"",
+                                lastName:"",
+                                email:"",
+                                isLocalBrewery:false,
+                                beerName:"",
+                                breweryName:""
+                            }}
 
-                    validationSchema={Yup.object().shape({
-                        firstName: Yup.string().required(),
-                        lastName: Yup.string().required(),
-                        email: Yup.string().email().required(),
-                        isLocalBrewery: Yup.boolean().required(),
-                        breweryName: Yup.string(),
-                        beerName: Yup.string().required(),                     
-                    })}
+                            validationSchema={Yup.object().shape({
+                                firstName: Yup.string().required(),
+                                lastName: Yup.string().required(),
+                                email: Yup.string().email().required(),
+                                isLocalBrewery: Yup.boolean().required(),
+                                breweryName: Yup.string(),
+                                beerName: Yup.string().required(),                     
+                            })}
 
-                    render={props =>(
-                        <div className="form-wrapper">
-                            <Form className="form-style">
-                                <h1>Recommend a Beer Form</h1>
-                                <label className="form-input-field">
-                                    First Name:
-                                    <Field name="firstName" placeholder="First Name"/>
-                                    <ErrorMessage name="firstName"/>
-                                </label>
-                                <label className="form-input-field">
-                                    Last Name:
-                                    <Field name="lastName" placeholder="Last Name"/>
-                                    <ErrorMessage name="lastName"/>
-                                </label>
-                                <label className="form-input-field">
-                                    Email Address:
-                                    <Field name="email" placeholder="Email Address"/>
-                                    <ErrorMessage name="email"/>
-                                </label>
-                                <label className="form-input-field">
-                                    Is beer sold by a local brewery?:
-                                    <Field type="checkbox" name="isLocalBrewery" checked="false"/>
-                                    <ErrorMessage name="isLocalBrewery"/>
-                                </label>
-                                <label className="form-input-field">
-                                    Brewery Name:
-                                    <Field name="breweryName" placeholder="Brewery Name"/>
-                                    <ErrorMessage name="breweryName"/>
-                                </label>
-                                <label className="form-input-field">
-                                    Beer Name:
-                                    <Field name="beerName" placeholder="Beer Name"/>
-                                    <ErrorMessage name="beerName"/>
-                                </label>
-                                <button type="submit" className="form-button">Submit</button>
-                            </Form>
-                        </div>
-                    )}
-                />
-                <Footer />
+                            render={props =>(
+                                <div>
+                                    <Form className="form-style">
+                                        <h1>Recommend a Beer Form</h1>
+                                        <label className="form-input-field">
+                                            First Name:
+                                            <Field name="firstName" placeholder="First Name"/>
+                                            <ErrorMessage name="firstName"/>
+                                        </label>
+                                        <label className="form-input-field">
+                                            Last Name:
+                                            <Field name="lastName" placeholder="Last Name"/>
+                                            <ErrorMessage name="lastName"/>
+                                        </label>
+                                        <label className="form-input-field">
+                                            Email Address:
+                                            <Field name="email" placeholder="Email Address"/>
+                                            <ErrorMessage name="email"/>
+                                        </label>
+                                        <label className="form-input-field">
+                                            Is beer sold by a local brewery?:
+                                            <Field type="checkbox" name="isLocalBrewery" checked="false"/>
+                                            <ErrorMessage name="isLocalBrewery"/>
+                                        </label>
+                                        <label className="form-input-field">
+                                            Brewery Name:
+                                            <Field name="breweryName" placeholder="Brewery Name"/>
+                                            <ErrorMessage name="breweryName"/>
+                                        </label>
+                                        <label className="form-input-field">
+                                            Beer Name:
+                                            <Field name="beerName" placeholder="Beer Name"/>
+                                            <ErrorMessage name="beerName"/>
+                                        </label>
+                                        <button type="submit" className="form-button">Submit</button>
+                                    </Form>
+                                </div>
+                            )}
+                        />
+                    </div>
+                    <Footer />
+                </div>
+                
             </Fragment>
         )
     }
